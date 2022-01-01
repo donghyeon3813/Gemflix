66bb3faeaf37ad1203323afe0f4c08e7e1f2b720
ChaeRin-Im <ewuuu1112@gmail.com>
2022-01-01 13:38:46 +0900

    Project Setting

---
+++ Gemflix/src/main/java/com/movie/Gemflix/GemflixApplication.java
@@ -0,0 +1,13 @@
+package com.movie.Gemflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemflixApplication.class, args);
+	}
+
+}
