5749c49febbd83a742db5c137aa664e6bd3bb07d
ChaeRin-Im <ewuuu1112@gmail.com>
2022-05-06 17:27:51 +0900

    MainPage Css

--- Gemflix-Front/src/components/home/home.jsx
+++ Gemflix-Front/src/components/home/home.jsx
@@ -7,9 +7,8 @@ const Home = ({server, onClickLogout }) => {
     return (
         <div className='home'>
             <Navbar server={server} onClickLogout={onClickLogout}/>
-            <div className='main'>
-                <h1>메인페이지</h1>
-            </div>
+            <img id='main' style={{width:"100%",height:"100%"}} src="images/default/main.jpg"/>
+            
         </div>
     );
 }
