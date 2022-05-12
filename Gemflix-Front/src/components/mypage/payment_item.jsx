0a26b36f366338bcca6cd9c20b77bb7b4667b5cc
ChaeRin-Im <ewuuu1112@gmail.com>
2022-05-12 14:24:21 +0900

    Modify Css

--- Gemflix-Front/src/components/mypage/payment_item.jsx
+++ Gemflix-Front/src/components/mypage/payment_item.jsx
@@ -17,38 +17,45 @@ const PaymentItem = (props) => {
   };
 
   return (
-    <div className="product_payment_item">
-      <div className="payment_item">
-        <h3>결제번호 : {payment.pmId}</h3>
-        <p>결제일시 : {String(payment.payDate).replace("T", " ")}</p>
-        <br/><hr/><br/>
+    <div>
+    {paidProducts.length > 0 ?
+      (
+        <div className="product_payment_item">
+          <div className="payment_item">
+            <h3>결제번호 : {payment.pmId}</h3>
+            <p>결제일시 : {String(payment.payDate).replace("T", " ")}</p>
+            <br/><hr/><br/>
 
-        {paidProducts.map((paidProduct, index) => {
-          return (
-            <div key={index}>
-              <div className="cart_item">
-                <div className="cart_img">
-                  <img
-                    className="preview"
-                    src={paidProduct.base64}
-                    style={{ width: "100px", height: "100px" }}
-                  />
+            {paidProducts.map((paidProduct, index) => {
+              return (
+                <div key={index}>
+                  <div className="cart_item">
+                    <div className="cart_img">
+                      <img
+                        className="preview"
+                        src={paidProduct.base64}
+                        style={{ width: "100px", height: "100px" }}
+                      />
+                    </div>
+                    <div className="cart_content">
+                      <h4>{paidProduct.product.name}</h4>
+                      <p>수량 : {paidProduct.count}</p>
+                      <p>가격 : {inputPriceFormat(paidProduct.price)} 원</p>
+                    </div>
+                  </div>
                 </div>
-                <div className="cart_content">
-                  <h4>{paidProduct.product.name}</h4>
-                  <p>수량 : {paidProduct.count}</p>
-                  <p>가격 : {inputPriceFormat(paidProduct.price)} 원</p>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-        
-        <br/><hr/><br/>
-        <p>상품금액 : {inputPriceFormat(payment.proAmount)} 원</p>
-        <p>할인금액 : -{inputPriceFormat(payment.disAmount)} 원</p>
-        <h3 className="total_price">결제금액 : {inputPriceFormat(payment.payAmount)} 원</h3>
-      </div>
+              );
+            })}
+            
+            <br/><hr/><br/>
+            <p>상품금액 : {inputPriceFormat(payment.proAmount)} 원</p>
+            <p>할인금액 : -{inputPriceFormat(payment.disAmount)} 원</p>
+            <h3 className="total_price">결제금액 : {inputPriceFormat(payment.payAmount)} 원</h3>
+          </div>
+        </div>
+      )
+    : 
+    null}
     </div>
   );
 };
--- Gemflix-Front/src/components/mypage/payment_list.jsx
+++ Gemflix-Front/src/components/mypage/payment_list.jsx
@@ -7,51 +7,44 @@ import { useLocation } from 'react-router';
 
 const PaymentList = () => {
 
-        const user = useSelector(store => store.userReducer, shallowEqual);
-        const location = useLocation();
-        const [disStatus, setDisStatus] = useState('P');
-        const payments = location.state.payments;
-        
-        const productDisplayType = disStatus === 'P' ? {display:"block"} : {display:"none"};
-        const ticketDisplayType = disStatus === 'T' ? {display:"block"} : {display:"none"};
-        const productBtnColor = disStatus === 'P' ? "indigo_btn" : "white_btn";
-        const ticketBtnColor = disStatus === 'T' ? "indigo_btn" : "white_btn";
-
-        
+  const user = useSelector(store => store.userReducer, shallowEqual);
+  const location = useLocation();
+  const [disStatus, setDisStatus] = useState('P');
+  const payments = location.state.payments;
+  
+  const productDisplayType = disStatus === 'P' ? {display:"block"} : {display:"none"};
+  const ticketDisplayType = disStatus === 'T' ? {display:"block"} : {display:"none"};
+  const productBtnColor = disStatus === 'P' ? "indigo_btn" : "white_btn";
+  const ticketBtnColor = disStatus === 'T' ? "indigo_btn" : "white_btn";
 
   const changeDisplay = (event) => {
     setDisStatus(event.target.value);
   };
+  
+  return (
+    <div className="payment_list_form">
+      <button className={productBtnColor} type="button" value="P" onClick={changeDisplay}>
+        구매한 상품
+      </button>
+      <button className={ticketBtnColor} type="button" value="T" onClick={changeDisplay}>
+        구매한 티켓
+      </button>
 
-  if (payments.length == 0) {
-    return <div>상품 결제내역이 없습니다.</div>;
-
-  } else {
-    return (
-      <div className="payment_list_form">
-        <button className={productBtnColor} type="button" value="P" onClick={changeDisplay}>
-          구매한 상품
-        </button>
-        <button className={ticketBtnColor} type="button" value="T" onClick={changeDisplay}>
-          구매한 티켓
-        </button>
-
-        {/* 상품 결제내역 */}
-        <div style={productDisplayType}>
-          {payments.map((payment) => {
-            return <PaymentItem key={payment.pmId} payment={payment} />;
-          })}
-        </div>
-
-        {/* 티켓 결제내역 */}
-        <div style={ticketDisplayType}>
-          {payments.map((payment) => {
-            return <PaymentTicketItem key={payment.pmId} payment={payment} />;
-          })}
-        </div>
+      {/* 상품 결제내역 */}
+      <div style={productDisplayType}>
+        {payments.map((payment) => {
+          return <PaymentItem key={payment.pmId} payment={payment} />;
+        })}
       </div>
-    );
-  }
+
+      {/* 티켓 결제내역 */}
+      <div style={ticketDisplayType}>
+        {payments.map((payment) => {
+          return <PaymentTicketItem key={payment.pmId} payment={payment} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default PaymentList;
